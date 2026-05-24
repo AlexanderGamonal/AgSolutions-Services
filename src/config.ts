4d400ae8bfab8e7815b207833e5d4e5caf6d2584
Alexander Gamonal <47549265+AlexanderGamonal@users.noreply.github.com>
2026-05-24 17:49:26 -0500

    Actualizar config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const WHATSAPP_NUMBER = '51XXXXXXXXX'
+export const WHATSAPP_NUMBER = '51942137116'
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}`
-export const EMAIL = 'alexmg.750@gmail.com'
+export const EMAIL = 'agsolutionsandservices@gmail.com'
